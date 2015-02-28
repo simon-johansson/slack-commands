9bf87018cced1bb9a194fd16ac823168d9f810ad
Simon Johansson <simon.johansson@screeninteraction.com>
2015-02-28 23:58:04 +0100

    Add /yoda endpoint
    Corresponds to Slack slash command '/yoda [sentence to yodafy]'
    
    Uses yodaspeak api to yodafy users input

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var lunch = require('./routes/lunch');
 var trains = require('./routes/trains');
+var yoda = require('./routes/yoda');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/lunch', lunch);
 app.use('/trains', trains);
+app.use('/yoda', yoda);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/yoda.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var request = require('request');
+var Slack = require('node-slack');
+
+var mashupKey = require('../secrets').keys.yoda.mashup;
+var slackKey = require('../secrets').keys.yoda.slack;
+
+var slack = new Slack('screeninteraction', slackKey);
+var router = express.Router();
+
+router.post('/', function(req, res) {
+
+  var text = req.body.text.split(' ').join('+');
+
+  var options = {
+    url: 'https://yoda.p.mashape.com/yoda?sentence=' + text,
+    headers: {
+      'User-Agent': 'request',
+      "X-Mashape-Key": mashupKey,
+      "Accept": "text/plain"
+    }
+  };
+
+  request(options, function(err, response, html) {
+
+    // No more integrations available at the moment, need to
+    // pay for Slack in order to be able to send JSON
+    var reply = slack.respond(req.body, function(hook) {
+      return {
+        text: response.body,
+        username: 'Yoda (' + hook.user_name + ')',
+        icon_emoji: 'yoda',
+      };
+    });
+
+    // res.json(reply);
+    res.send(response.body);
+  });
+});
+
+module.exports = router;
